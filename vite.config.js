f41f7c3f9b47e4065175a237b736edf865417ecf
Shrushti2005-code <it23.shrushti.deshmukh@kccemsr.edu.in>
2026-03-19 22:06:35 +0530

    Fix: update base path to root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TESTIFY/",
+  // Setting base to "/" allows the site to load at shrushti2005-code.github.io
+  base: "/", 
 });
