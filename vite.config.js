03f5ef16912ad229d2d2ee88aaf70f9143c27c06
Shrushti2005-code <it23.shrushti.deshmukh@kccemsr.edu.in>
2026-03-19 15:42:31 +0530

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Required for GitHub Pages deployment
-  // If your repo is: github.com/username/testify
-  // then base should be: "/testify/"
-  // If using Vercel/Netlify, keep base as "/"
-  base: "/",
+  base: "/TESTIFY/",
 });
